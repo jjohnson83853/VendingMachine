3600e93a9bc00d5a93d3c00c38aff14aa4c504f4
jjohnson83853 <bullprog3@gmail.com>
2016-01-17 16:08:58 -0500

    Can now display total money.

--- src/main/java/VendingMachine.java
+++ src/main/java/VendingMachine.java
@@ -1,16 +1,23 @@
+import java.text.NumberFormat;
+
 /**
  * Created by bullprog3 on 1/17/16.
  */
 public class VendingMachine {
 
-    public void pressButton(String button) {
+    private double totalMoney;
 
+    public void pressButton(String button) {
     }
 
     public String getDisplay() {
+        if (totalMoney > 0.00) {
+            return NumberFormat.getCurrencyInstance().format(totalMoney);
+        }
         return "COLA SELECTED.";
     }
 
     public void insertMoney(double money) {
+        ++totalMoney;
     }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
  * Created by bullprog3 on 1/17/16.
  */
 public class VendingMachineTest {
+
     @Test
     public void selectCola() {
         final VendingMachine myVendingMachine = new VendingMachine();
@@ -22,6 +23,7 @@ public class VendingMachineTest {
         assertEquals("COLA SELECTED.", myVendingMachine.getDisplay());
         try {
             myVendingMachine.insertMoney(1.00);
+            assertEquals("$1.00", myVendingMachine.getDisplay());
         } catch (RuntimeException re) {
             fail();
         }
